43b2f59d270620910ad737b9d5581fca87d0301b
markusohlsson <makkeytv@gmail.com>
2025-08-13 15:13:00 +0200

    removed aos from main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,15 +2,7 @@ import { createApp } from 'vue'
 import './assets/main.css'
 import App from './App.vue'
 import router from './router'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import { createPinia } from 'pinia';
 const pinia = createPinia();
-AOS.init({
-    duration: 800,
-    offset: 150,
-    once: false,
-    mirror: true,
-})
 
 createApp(App).use(router).use(pinia).mount('#app')
